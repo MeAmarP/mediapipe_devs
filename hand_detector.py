0228705502be787d276925a6501e76abb3c27b66
meamarp <appotdar05@gmail.com>
2022-03-05 01:17:51 +0530

    code to add annotations on the input frame.

--- hand_detector.py
+++ hand_detector.py
@@ -26,12 +26,17 @@ class HandGestureDetector:
     self.down_count_done_flag = False
     self.fin_flag = False
 
+
     self.CountMaintainer = {'THUMB_UP': 0, 'THUMB_DOWN':0}
 
-    mpHands = mp.solutions.hands
-    self.HandDets = mpHands.Hands(static_image_mode=True,
+    self.mpHands = mp.solutions.hands
+    self.HandDets = self.mpHands.Hands(static_image_mode=True,
                                   max_num_hands=maxHands,
                                   min_detection_confidence=minConfScore)
+
+    self.mp_draw = mp.solutions.drawing_utils
+    self.mp_drawing_styles = mp.solutions.drawing_styles
+
 
   def checkThumbsUpDown(self, img_frame):
     """
@@ -43,18 +48,26 @@ class HandGestureDetector:
     """
     
     self.IMG_HT,self.IMG_WT = img_frame.shape[:2]
-    img_frame = cv2.flip(img_frame, 1)
+    # img_frame = cv2.flip(img_frame, 1)
     img_frame = cv2.cvtColor(img_frame, cv2.COLOR_BGR2RGB)
+    self.annotated_img = img_frame.copy()
 
     self.results = self.HandDets.process(img_frame)
 
     if self.results.multi_hand_landmarks:
       for h_lmarks in self.results.multi_hand_landmarks:
+
+        self.mp_draw.draw_landmarks(self.annotated_img, h_lmarks,self.mpHands.HAND_CONNECTIONS,
+                                    self.mp_drawing_styles.get_default_hand_landmarks_style(),
+                                    self.mp_drawing_styles.get_default_hand_connections_style())
+
         thumb_tip_val = h_lmarks.landmark[4]
         index_tip_val = h_lmarks.landmark[8]
         middle_tip_val = h_lmarks.landmark[12]
         ring_tip_val = h_lmarks.landmark[16]
         pinky_tip_val = h_lmarks.landmark[20]
+
+
 
         if thumb_tip_val.y < index_tip_val.y < middle_tip_val.y < ring_tip_val.y < pinky_tip_val.y:          
           self.gesture_assert_counter += 1
@@ -65,6 +78,8 @@ class HandGestureDetector:
           self.gesture_assert_counter += 1
           if self.gesture_assert_counter >= self.ASSERT_THRESH and (self.down_count_done_flag == False):
             self.down_count_done_flag = True
+
+
     else:
       self.up_count_done_flag = False
       self.down_count_done_flag = False
@@ -80,7 +95,7 @@ class HandGestureDetector:
       self.CountMaintainer['THUMB_DOWN'] = self.CountMaintainer['THUMB_DOWN'] + 1
       self.fin_flag = True
 
-    return self.CountMaintainer
+    return self.CountMaintainer, self.annotated_img 
 
 if __name__ == "__main__":
     import argparse
@@ -91,6 +106,9 @@ if __name__ == "__main__":
 
     #Test class on Video
     vidcap = cv2.VideoCapture(args.videosrc)
+
+    fourcc = cv2.VideoWriter_fourcc(*'MPEG')
+    v_write = cv2.VideoWriter('mediapipe_fun1.mp4', fourcc, 10.0,(1280,480))
    
     handgestvid = HandGestureDetector(maxHands=1)
 
@@ -106,12 +124,18 @@ if __name__ == "__main__":
         print("End of the Video")
         break    
 #      frame = cv2.resize(frame,(416,416))
-      thumb_count = handgestvid.getThumbCount(frame)
+      thumb_count, annon_img = handgestvid.getThumbCount(frame)
+      
 
+      cv2.putText(frame, "  Likes    " +str(thumb_count['THUMB_UP']), (310,30),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
+      cv2.putText(frame, "Dis-Likes  " +str(thumb_count['THUMB_DOWN']), (310,60),cv2.FONT_HERSHEY_SIMPLEX, 0.7,(255,0,0), 2)
 
-      cv2.putText(frame, "Likes = " +str(thumb_count['THUMB_UP']), (10,40),cv2.FONT_HERSHEY_SIMPLEX, 0.6,(128,255,0), 2)
-      cv2.putText(frame, "Dis-Likes = " +str(thumb_count['THUMB_DOWN']), (10,60),cv2.FONT_HERSHEY_SIMPLEX, 0.6,(128,255,0), 2)
-      cv2.imshow('Output', frame)
+      fin_frame = cv2.hconcat([frame, cv2.cvtColor(annon_img, cv2.COLOR_BGR2RGB)])
+      print(fin_frame.shape)
+      v_write.write(fin_frame)
+      cv2.imshow('Output', fin_frame)
+      # cv2.imshow('Output1',)
+      
       
       if cv2.waitKey(1) == ord('q'):
           break
